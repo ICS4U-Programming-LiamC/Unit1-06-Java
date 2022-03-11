fc3070cbce816ccf28ad7c2bacedf92da4299c0c
Liam Csiffary <liam.csiffary@stu.ocsb.ca>
2022-03-11 08:23:52 -0500

    finished java modeMedianMean

--- modeMedianMean.java
+++ modeMedianMean.java
@@ -1,103 +1,121 @@
+/**
+* The modeMedianMean calculator finds the mode median and mean
+* of a given txt file filled with numbers
+*
+* @author  Mr. Coxall
+* @version 1.0
+* @since   2021-11-16
+*/
+
 import java.util.*;
 import java.io.File;
 
 // main class
-class modeMedianMean {   
+class modeMedianMean {
     // mean calculator
     public static float Mean(int arrayMean[]) {
         // define vars
         float mean;
         float total = 0;
+
         // adds up all the number in the file
         for (int index = 0; index < arrayMean.length; index++) {
             total += arrayMean[index];
         }
+
         // Divide by the number of numbers
         mean = total / arrayMean.length;
         return mean;
     }
 
     // median calculator
-    public static float Median(List<Integer> sortedArrayMedian) {
+    public static float Median(int sortedArrayMedian[]) {
         // define vars
         float median = 0;
 
-        // continues to remove the outsides until theres 1 or 2 numbers left
-        while (sortedArrayMedian.size() > 2) {
-            sortedArrayMedian.remove(0);
-            sortedArrayMedian.remove(sortedArrayMedian.size()-1);
-        }
-        // System.out.println(sortedArray + " Median");
-
-        // if theres two medians calculate the average of the two
-        if (sortedArrayMedian.size() == 2) {
-            median = (sortedArrayMedian.get(0) + sortedArrayMedian.get(1)) / 2;
+        // finds the middle of the array and sets that/those number(s) to the median
+        if (sortedArrayMedian.length % 2 == 0) {
+            median = sortedArrayMedian[sortedArrayMedian.length / 2];
+        } else {
+            median = (sortedArrayMedian[(sortedArrayMedian.length / 2) - 1]
+                    + sortedArrayMedian[(sortedArrayMedian.length)]) / 2;
         }
 
         return median;
     }
 
     // mode calculator
-    public static List<Integer> Mode(List<Integer> sortedArrayMode) {
+    public static List<Integer> Mode(int sortedArrayMode[]) {
         // define vars
         List<Integer> modes = new ArrayList<Integer>();
         int currentCounter = 0;
         int highestCounter = 0;
+
         // set currentNum to count
-        int currentNum = sortedArrayMode.get(0);
+        int currentNum = sortedArrayMode[0];
+
+        // creates a new array one larger than the old one
+        // populates it with the same numbers but with a -1 at the end
+        int sortedArrayPlusOne[] = new int[sortedArrayMode.length + 1];
+        for (int index = 0; index < sortedArrayMode.length; index++) {
+            sortedArrayPlusOne[index] = sortedArrayMode[index];
+        }
+        sortedArrayPlusOne[sortedArrayMode.length] = -1;
 
         // counts how many of each number there is
-        for (int index = 0; index < sortedArrayMode.size(); index++) {
+        for (int index = 0; index < sortedArrayMode.length; index++) {
+
             // if the number its looking for is the number at that index then
-            // add 1 to the counter 
-            if (currentNum == sortedArrayMode.get(index)) {
+            // add 1 to the counter
+            if (currentNum == sortedArrayMode[index]) {
                 currentCounter += 1;
+
             } else {
                 // once not true we check if its the number that has appeared the most
                 // if it is empty the old list and add the current num
                 if (currentCounter > highestCounter) {
                     highestCounter = currentCounter;
                     modes.clear();
-                    modes.add(sortedArrayMode.get(index - 1));
+                    modes.add(sortedArrayMode[index - 1]);
                 }
+
                 // if it appears the same number of times then
-                // add it to the list only 
+                // add it to the list only
                 else if (currentCounter == highestCounter) {
-                    modes.add(sortedArrayMode.get(index - 1));
+                    modes.add(sortedArrayMode[index - 1]);
                 }
+
                 // set the new num to count for
-                currentNum =  sortedArrayMode.get(index);
+                currentNum = sortedArrayMode[index];
+
                 // set the number of times that number has appeared to 1
                 currentCounter = 1;
             }
         }
         return modes;
     }
-    
+
+    // seems to need the exception or the Scanner(file) complains
     public static void main(String[] args) throws Exception {
+
         // defining vars
         List<String> originalListString = new ArrayList<String>();
-        File file = new File("./Unit1/Java/Unit1-06-Java/set3.txt");
+        File file = new File("./Unit1/Java/Unit1-06-Java/set1.txt");
         Scanner sc = new Scanner(file);
-
-        // for the sorted array
-        int numToLookFor = 0;
 
         // for my inificient code to sort
         int biggestInt = 0;
 
         // pass the path to the file as a parameter
-        while (sc.hasNextLine())
-        {
+        while (sc.hasNextLine()) {
             originalListString.add(sc.nextLine());
         }
 
         System.out.println(originalListString);
 
         // List<Integer> listInt = new ArrayList<Integer>();
-        int arrayInt[] = new int [originalListString.size()];
-
-        List<Integer> sortedArray = new ArrayList<Integer>();
+        int arrayInt[] = new int[originalListString.size()];
+        int sortedArray[] = new int[originalListString.size()];
 
         // converts files list to a list of ints and checks for errors
         for (int index = 0; index < originalListString.size(); index++) {
@@ -110,16 +128,21 @@ class modeMedianMean {
         }
 
         // these two sort the list
+        // finds the biggest number in the array
         for (int index = 0; index < arrayInt.length; index++) {
             if (arrayInt[index] > biggestInt) {
                 biggestInt = arrayInt[index];
             }
         }
 
+        // for the sorted array
+        int numToLookFor = 0;
+        int counter = 0;
         while (numToLookFor <= biggestInt) {
             for (int index = 0; index < arrayInt.length; index++) {
                 if (numToLookFor == arrayInt[index]) {
-                    sortedArray.add(numToLookFor);
+                    sortedArray[counter] = numToLookFor;
+                    counter += 1;
                 }
             }
             numToLookFor += 1;
